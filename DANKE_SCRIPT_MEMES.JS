4230f165da2c48992c5f4b3afef08b18f9c337ad
testaccount123312 <Nope.cs.cn@gmail.com>
2017-02-25 16:12:57 -0500

    Initial Upload

---
+++ DANKE_SCRIPT_MEMES.JS
@@ -0,0 +1,11 @@
+var button = document.getElementById('button'); // Assumes element with id='button'
+
+button.onclick = function() {
+    var div = document.getElementById('newpost');
+    if (div.style.display !== 'none') {
+        div.style.display = 'none';
+    }
+    else {
+        div.style.display = 'block';
+    }
+};
